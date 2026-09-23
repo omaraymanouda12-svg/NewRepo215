21673cdfc0aa6f248a9ff273ca681cff8b126c27
omaraymanouda12-svg <omaraymanouda12@gmail.com>
2026-09-23 06:51:49 -0700

    /Question 4 ANSWER

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -91,3 +91,93 @@ public class DeliveryCenter
 
 
 //Question 3 ANSWER
+
+
+
+
+namespace SmartDeliveryManagement
+{
+
+    public struct DeliveryAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+
+        public DeliveryAddress(string street, string city)
+        {
+            Street = street;
+            City = city;
+        }
+    }
+
+    public class Shipment
+    {
+        public string TrackingCode { get; set; }
+        public string Description { get; set; }
+        public decimal Weight { get; set; }
+        public decimal DeliveryFee { get; set; }
+        public DeliveryAddress Destination { get; set; }
+
+       
+        public decimal EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5); }
+        }
+
+     
+        public Shipment()
+        {
+        }
+
+       
+        public Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            TrackingCode = trackingCode;
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+
+       
+        public void UpdateDeliveryFee(decimal newFee)
+        {
+            DeliveryFee = newFee;
+        }
+
+      
+        public void PrintShipment()
+        {
+            Console.WriteLine($"Tracking Code: {TrackingCode}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Delivery Fee: {DeliveryFee}");
+            Console.WriteLine($"Destination: {Destination.City}, {Destination.Street}");
+        }
+    }
+
+   
+    public class DeliveryCenter
+    {
+        public string CenterName { get; set; }
+        public DeliveryAddress Location { get; set; }
+
+        public DeliveryCenter()
+        {
+        }
+
+        public DeliveryCenter(string centerName, DeliveryAddress location)
+        {
+            CenterName = centerName;
+            Location = location;
+        }
+
+        public void PrintCenter()
+        {
+            Console.WriteLine($"Center Name: {CenterName}");
+            Console.WriteLine($"Location: {Location.City}, {Location.Street}");
+        }
+    }
+}
+
+//Question 4 ANSWER
