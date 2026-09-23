b5e094f7a10a484480e182c89f1ac87f17f457f4
omaraymanouda12-svg <omaraymanouda12@gmail.com>
2026-09-23 06:35:27 -0700

    Question 1 ANSWER

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Question 1
+//Answer the following questions:
+//a) What is the difference between a class and a struct?
+//b) Why are classes more suitable than structs for large applications?
+
+//A => Memory/Type: A class is a reference type (stored on the heap), while a struct is a value type (stored on the stack.
+
+//Default Visibility(C++): Struct members are public by default, whereas class members are private by default.
+
+// B => Performance & Copying: Classes use reference semantics, avoiding the heavy performance cost of copying large blocks of data that structs (value types) incur.
+
+//Object - Oriented Features: Classes fully support inheritance, polymorphism, and complex hierarchies, which are essential for scalable, maintainable architectures.
+
+//Question 1 ANSWER
+
